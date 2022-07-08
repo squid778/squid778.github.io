29e30a7bce147873592831566b0e99b027196bbd
squid778 <squid778@gmail.com>
2022-07-08 16:46:48 +0700

    test

--- sort.js
+++ sort.js
@@ -21,8 +21,8 @@ for(let i = 0; i < th.length; i++)
 
     /* tell table which column is currently sorted */
     table.dataset.sort = e.target.cellIndex;
-    sortColumn(i);
-    //sortTable(i);
+    //sortColumn(i);
+    sortTable(i);
   });
 }
 }
@@ -80,13 +80,13 @@ function sortTable(n) {
       /* Check if the two rows should switch place,
       based on the direction, asc or desc: */
       if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+        if (Number(x.innerHTML.toLowerCase()) > Number(y.innerHTML.toLowerCase())) {
           // If so, mark as a switch and break the loop:
           shouldSwitch = true;
           break;
         }
       } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+        if (Number(x.innerHTML.toLowerCase()) < Number(y.innerHTML.toLowerCase())) {
           // If so, mark as a switch and break the loop:
           shouldSwitch = true;
           break;
